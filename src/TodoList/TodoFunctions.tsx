5c4d6a470d796ee7bc1b395a4678aa19d0a36bd1
farishikomet <farish.j@ikomet.com>
2023-07-04 02:58:51 +0530

    Revert "todo with delete button"
    This reverts commit 1c7dd9c8b3de0ff2e5eb450d6fdc9856e91f3aa2.

--- src/TodoList/TodoFunctions.tsx
+++ src/TodoList/TodoFunctions.tsx
@@ -1,21 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-interface TodoState {
-    data: string[];
-  }
 
 const TodoFunctions = createSlice({
     name:'todo',
-    initialState: { data: [] } as TodoState,
+    initialState:{data:''},
     reducers:{
        listinput:(state,action)=>{
-        state.data.push(action.payload);
-       },
-       listremove:(state,action)=>{
-       state.data = state.data.filter(a => a != action.payload)
+        state.data = action.payload
        } 
     }
 });
 
-export const { listinput,listremove } = TodoFunctions.actions;
+export const { listinput } = TodoFunctions.actions;
 export default TodoFunctions.reducer;
--- src/TodoList/TodoMain.tsx
+++ src/TodoList/TodoMain.tsx
@@ -1,6 +1,6 @@
 import React,{ useEffect, useState } from 'react';
 import {useDispatch,useSelector} from 'react-redux'
-import { listinput, listremove } from './TodoFunctions';
+import { listinput } from './TodoFunctions';
 
 
 
@@ -8,7 +8,7 @@ const TodoMain = () => {
   
     const [todoinput, settodoinput] = useState('');
     const dispatch = useDispatch();
-    const todoData = useSelector((state:{todostore:{data:string[]}})=>state.todostore.data);   
+    const todoData = useSelector((state:any)=>state.todostore.data);   
 
     useEffect(() => {
         console.log(todoData);
@@ -17,22 +17,17 @@ const TodoMain = () => {
 
 
     return (
-    <div>
-TodoMain
+    <div>TodoMain
         <input 
         value={todoinput}
         onChange={(e)=>{settodoinput(e.target.value)}}
         />
-        <button style={{'backgroundColor':'blue'}} onClick={()=>{
-            dispatch(listinput(todoinput))
+        <button onClick={()=>{
+            dispatch(listinput(String(todoinput || '')))
             settodoinput('');
             }}>list</button>
-        {todoData.map((todo,index)=>(
-            <ul>
-                <li key={index}>{todo}</li>
-                <button onClick={()=>{dispatch(listremove(todo))}}>delete</button>
-            </ul>
-        ))}
+        {todoData}
+        {/* <div>{todoData && todoData.length > 0 ? todoData : ''}</div> */}
     </div>
   )
 }
